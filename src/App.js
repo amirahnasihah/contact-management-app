05d4847448e1c05e875d276b4054e90f15101065
amirahnasihah <amirahnasihah97@gmail.com>
2023-07-19 13:53:31 +0800

    prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ContactList from "./components/ContactList";
 import AddContact from "./components/AddContact";
